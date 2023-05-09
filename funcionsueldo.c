64497a6144bc8969f112f31144acc445eb385cf6
jojoshtin <126208677+jojoshtin@users.noreply.github.com>
2023-05-08 23:33:35 -0400

    Add files via upload

---
+++ funcionsueldo.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+struct trabajador
+{
+    int cedula;
+    char nombre[50];
+    int departamento;
+    int cargo;
+    float sueldo;
+    char f_ingreso[10];
+};
+int vectorindices[100];
+void indicesueldo(float sueldolocal,struct trabajador vector[],int tam,int vectorindices[]);
+float sueldolocal;
+
+void indicesueldo(float sueldolocal,struct trabajador vector[],int tam,int vectorindices[]){
+    int i, j=0;
+    for (i=0;i<tam;i++){
+        if (vector[i].sueldo=sueldolocal){
+            vectorindices[j]=i;
+            j++;
+        }
+    }
+}
